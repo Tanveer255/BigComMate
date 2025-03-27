b55e2eae39d85ec90b81e16f9493431f3dd20b1e
Muhammad Tanveer <tanveerofficial244@gmail.com>
2025-03-27 07:32:10 +0500

    WebhookController added in and uninstallpayload added

---
+++ BigComMate/Controllers/WebhookController.cs
@@ -0,0 +1,18 @@
+﻿using BigComMate.Entity.Common.Model;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BigComMate.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class WebhookController : ControllerBase
+{
+    [HttpPost("uninstall")]
+    public IActionResult Uninstall([FromBody] UninstallPayload payload)
+    {
+        // Remove store data from DB
+        return Ok();
+    }
+}
---
+++ BigComMate/Entity/Common/Model/UninstallPayload.cs
@@ -0,0 +1,8 @@
+﻿namespace BigComMate.Entity.Common.Model;
+
+public class UninstallPayload
+{
+    public string Store_Hash { get; set; }  // Unique identifier for the store
+    public string Client_Id { get; set; }   // App’s client ID
+    public string User_Id { get; set; }     // ID of the user who uninstalled the app
+}
